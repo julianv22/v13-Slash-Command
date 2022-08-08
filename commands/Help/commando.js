ed034a0885c641a89b59c1127aacdbfbab9fe39e
Julian-V <102874804+julianv22@users.noreply.github.com>
2022-08-08 17:40:25 +0000

    12.40-09.08.22

--- commands/Help/commando.js
+++ commands/Help/commando.js
@@ -7,33 +7,45 @@ exports.description = `⤷Danh sách Slash Command\n\nAlias: \`${exports.aliases
 exports.usage = `\`${cfg.prefix}${exports.name}\``;
 
 exports.execute = async (message, args, client) => {
-  if (args.join(' ').trim() === '?') return client.cmdGuide(message, exports.name, exports.description, exports.usage);
+  if (args.join(" ").trim() === "?")
+    return client.cmdGuide(
+      message,
+      exports.name,
+      exports.description,
+      exports.usage
+    );
 
-  let slashCmd = [];
-  const cmdCategories = await client.slashCommands.map(cmd => cmd.category)
-  const catFilter = await cmdCategories.filter((item, index) => cmdCategories.indexOf(item) === index)
+  const cmdCategories = await client.slashCommands.map((cmd) => cmd.category);
+  const catFilter = await cmdCategories.filter(
+    (item, index) => cmdCategories.indexOf(item) === index
+  );
 
   const user = message.author;
   const embed = new MessageEmbed()
     .setAuthor(user.username, user.displayAvatarURL(true))
-    .setTitle('Danh sách các Slash Command (/) hiện có:')
-    .setDescription(`Tổng số command: [${cmdCategories.length}]`)
-    .setThumbnail('https://cdn.discordapp.com/attachments/976364997066231828/1001524699941064745/slash.png')
+    .setTitle("Slash Command (/) List:")
+    .addField(`Total comands: [${cmdCategories.length}]`, `\u200b`)
     .setFooter(message.guild.name, message.guild.iconURL(true))
     .setTimestamp()
+    .setColor(cfg.embedcolor)
+    .setDescription(`I you need some help, join my support server: [\`🦸〔J-V Bot〕 SUPPORT\`](https://discord.gg/dyd8DXbrVq)`)
+    .setThumbnail("https://cdn.discordapp.com/attachments/976364997066231828/1001524699941064745/slash.png");
 
   for (const cat of catFilter) {
-    const commands = await client.slashCommands.map(cmd => cmd).filter(cmd => cmd.category === cat);
-    embed.addField(`${cfg.folder} ${cat.toUpperCase()} [${commands.length}]`, `\u200b`);
-    slashCmd = commands.map(cmd => {
-      return {
-        name: cmd.data.name,
-        value: `⤷${cmd.data.description}`,
-        inline: true
-      }
-    });
-    embed.addFields(slashCmd);
+    const commands = await client.slashCommands
+      .map((cmd) => cmd)
+      .filter((cmd) => cmd.category === cat);
+    const cmds = await commands
+      .map((cmd) => {
+        return `⤷[${cmd.data.name}]⟶ ${cmd.data.description}`;
+      })
+      .join("\n");
+
+    embed.addField(
+      `${cfg.folder} ${cat.toUpperCase()} [${commands.length}]`,
+      `\`\`\`${cmds}\`\`\``
+    );
     continue;
-  };
+  }
   message.reply({ embeds: [embed] });
-}
+};
--- slashcommands/1 - Tools/commando.js
+++ slashcommands/1 - Tools/commando.js
@@ -7,40 +7,37 @@ module.exports = {
     .setDescription("Slash Commands (/) List"),
   category: "help",
   async execute(interaction, client) {
-    let cmds = [];
     const cmdCategories = await client.slashCommands.map((cmd) => cmd.category);
     const catFilter = await cmdCategories.filter(
       (item, index) => cmdCategories.indexOf(item) === index
     );
 
-    for (const cat of catFilter) {
-      const cmd = await client.slashCommands
-        .map((cmd) => cmd)
-        .filter((cmd) => cmd.category === cat);
-      cmds.push({
-        name: `${cfg.folder} ${cat.toUpperCase()} [${cmd.length}]`,
-        value: `\`\`\`${cmd.map((cmd) => cmd.data.name).join(" | ")}\`\`\``,
-      });
-    }
-
     const embed = new MessageEmbed()
-      .setAuthor(
-        interaction.member.user.username,
-        interaction.member.user.displayAvatarURL(true)
-      )
+      .setAuthor(interaction.user.username, interaction.user.displayAvatarURL(true))
       .setTitle("Slash Command (/) List:")
+      .addField(`Total comands: [${cmdCategories.length}]`, `\u200b`)
       .setFooter(interaction.guild.name, interaction.guild.iconURL(true))
       .setTimestamp()
       .setColor(cfg.embedcolor)
-      .addField(`Total comands: [${cmdCategories.length}]`, `\u200b`)
-      .addFields(cmds)
-      .setDescription(
-        `I you need some help, join my support server: [\`🎭〔J-V Bot〕 SUPPORT\`](https://discord.gg/dyd8DXbrVq)`
-      )
-      .setThumbnail(
-        "https://cdn.discordapp.com/attachments/976364997066231828/1001524699941064745/slash.png"
-      );
+      .setDescription(`I you need some help, join my support server: [\`🦸〔J-V Bot〕 SUPPORT\`](https://discord.gg/dyd8DXbrVq)`)
+      .setThumbnail("https://cdn.discordapp.com/attachments/976364997066231828/1001524699941064745/slash.png");
 
+    for (const cat of catFilter) {
+      const commands = await client.slashCommands
+        .map((cmd) => cmd)
+        .filter((cmd) => cmd.category === cat);
+      const cmds = await commands
+        .map((cmd) => {
+          return `⤷[${cmd.data.name}]⟶ ${cmd.data.description}`;
+        })
+        .join("\n");
+
+      embed.addField(
+        `${cfg.folder} ${cat.toUpperCase()} [${commands.length}]`,
+        `\`\`\`${cmds}\`\`\``
+      );
+      continue;
+    }
     interaction.reply({ embeds: [embed] });
   },
 };
